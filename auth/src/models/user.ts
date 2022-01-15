501155ddcb681c771061341d6739d4ee1bfbd3d5
yasith wimukthi <it19966922@my.sliit.lk>
2022-01-15 12:11:41 +0530

    edit user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,33 +1,48 @@
 import mongoose from 'mongoose';
 import { Password } from '../services/password';
 
-// An interface that describes the properties that are required to create a new User
+// An interface that describes the properties
+// that are requried to create a new User
 interface UserAttrs {
   email: string;
   password: string;
 }
 
-// An interface that describes the properties  that a User Model has
+// An interface that describes the properties
+// that a User Model has
 interface UserModel extends mongoose.Model<UserDoc> {
   build(attrs: UserAttrs): UserDoc;
 }
 
-// An interface that describes the properties that a User Document has
+// An interface that describes the properties
+// that a User Document has
 interface UserDoc extends mongoose.Document {
   email: string;
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
   },
-  password: {
-    type: String,
-    required: true
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      }
+    }
   }
-});
+);
 
 userSchema.pre('save', async function(done) {
   if (this.isModified('password')) {
